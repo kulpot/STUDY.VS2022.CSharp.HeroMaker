2a1015499ca188008c6e4d6c6e1616d15172494e
Kulpot <kulpotwahu@gmail.com>
2024-02-10 20:16:36 +0800

    added button name Create Hero

--- HeroMaker/Form1.Designer.cs
+++ HeroMaker/Form1.Designer.cs
@@ -31,14 +31,15 @@
             this.label1 = new System.Windows.Forms.Label();
             this.txt_name = new System.Windows.Forms.TextBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
-            this.checkBox3 = new System.Windows.Forms.CheckBox();
-            this.checkBox4 = new System.Windows.Forms.CheckBox();
-            this.checkBox5 = new System.Windows.Forms.CheckBox();
-            this.checkBox6 = new System.Windows.Forms.CheckBox();
-            this.checkBox7 = new System.Windows.Forms.CheckBox();
-            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.chk_fly = new System.Windows.Forms.CheckBox();
+            this.chk_xray = new System.Windows.Forms.CheckBox();
+            this.chk_invisible = new System.Windows.Forms.CheckBox();
+            this.chk_energy = new System.Windows.Forms.CheckBox();
+            this.chk_luck = new System.Windows.Forms.CheckBox();
+            this.chk_fart = new System.Windows.Forms.CheckBox();
+            this.chk_water_breath = new System.Windows.Forms.CheckBox();
+            this.chk_timecontrol = new System.Windows.Forms.CheckBox();
+            this.btn_create = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -60,14 +61,14 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.checkBox5);
-            this.groupBox1.Controls.Add(this.checkBox6);
-            this.groupBox1.Controls.Add(this.checkBox7);
-            this.groupBox1.Controls.Add(this.checkBox8);
-            this.groupBox1.Controls.Add(this.checkBox4);
-            this.groupBox1.Controls.Add(this.checkBox3);
-            this.groupBox1.Controls.Add(this.checkBox2);
-            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.chk_luck);
+            this.groupBox1.Controls.Add(this.chk_fart);
+            this.groupBox1.Controls.Add(this.chk_water_breath);
+            this.groupBox1.Controls.Add(this.chk_timecontrol);
+            this.groupBox1.Controls.Add(this.chk_energy);
+            this.groupBox1.Controls.Add(this.chk_invisible);
+            this.groupBox1.Controls.Add(this.chk_xray);
+            this.groupBox1.Controls.Add(this.chk_fly);
             this.groupBox1.Location = new System.Drawing.Point(15, 32);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(327, 228);
@@ -75,91 +76,101 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Special Abilities";
             // 
-            // checkBox1
+            // chk_fly
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(6, 28);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(39, 17);
-            this.checkBox1.TabIndex = 0;
-            this.checkBox1.Text = "Fly";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.chk_fly.AutoSize = true;
+            this.chk_fly.Location = new System.Drawing.Point(6, 28);
+            this.chk_fly.Name = "chk_fly";
+            this.chk_fly.Size = new System.Drawing.Size(39, 17);
+            this.chk_fly.TabIndex = 0;
+            this.chk_fly.Text = "Fly";
+            this.chk_fly.UseVisualStyleBackColor = true;
             // 
-            // checkBox2
+            // chk_xray
             // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(6, 51);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(81, 17);
-            this.checkBox2.TabIndex = 1;
-            this.checkBox2.Text = "xRay Vision";
-            this.checkBox2.UseVisualStyleBackColor = true;
+            this.chk_xray.AutoSize = true;
+            this.chk_xray.Location = new System.Drawing.Point(6, 51);
+            this.chk_xray.Name = "chk_xray";
+            this.chk_xray.Size = new System.Drawing.Size(81, 17);
+            this.chk_xray.TabIndex = 1;
+            this.chk_xray.Text = "xRay Vision";
+            this.chk_xray.UseVisualStyleBackColor = true;
             // 
-            // checkBox3
+            // chk_invisible
             // 
-            this.checkBox3.AutoSize = true;
-            this.checkBox3.Location = new System.Drawing.Point(6, 74);
-            this.checkBox3.Name = "checkBox3";
-            this.checkBox3.Size = new System.Drawing.Size(70, 17);
-            this.checkBox3.TabIndex = 2;
-            this.checkBox3.Text = "Invisibility";
-            this.checkBox3.UseVisualStyleBackColor = true;
+            this.chk_invisible.AutoSize = true;
+            this.chk_invisible.Location = new System.Drawing.Point(6, 74);
+            this.chk_invisible.Name = "chk_invisible";
+            this.chk_invisible.Size = new System.Drawing.Size(70, 17);
+            this.chk_invisible.TabIndex = 2;
+            this.chk_invisible.Text = "Invisibility";
+            this.chk_invisible.UseVisualStyleBackColor = true;
             // 
-            // checkBox4
+            // chk_energy
             // 
-            this.checkBox4.AutoSize = true;
-            this.checkBox4.Location = new System.Drawing.Point(6, 97);
-            this.checkBox4.Name = "checkBox4";
-            this.checkBox4.Size = new System.Drawing.Size(95, 17);
-            this.checkBox4.TabIndex = 3;
-            this.checkBox4.Text = "Absorb Energy";
-            this.checkBox4.UseVisualStyleBackColor = true;
+            this.chk_energy.AutoSize = true;
+            this.chk_energy.Location = new System.Drawing.Point(6, 97);
+            this.chk_energy.Name = "chk_energy";
+            this.chk_energy.Size = new System.Drawing.Size(95, 17);
+            this.chk_energy.TabIndex = 3;
+            this.chk_energy.Text = "Absorb Energy";
+            this.chk_energy.UseVisualStyleBackColor = true;
             // 
-            // checkBox5
+            // chk_luck
             // 
-            this.checkBox5.AutoSize = true;
-            this.checkBox5.Location = new System.Drawing.Point(126, 28);
-            this.checkBox5.Name = "checkBox5";
-            this.checkBox5.Size = new System.Drawing.Size(91, 17);
-            this.checkBox5.TabIndex = 7;
-            this.checkBox5.Text = "Extreme Luck";
-            this.checkBox5.UseVisualStyleBackColor = true;
+            this.chk_luck.AutoSize = true;
+            this.chk_luck.Location = new System.Drawing.Point(126, 28);
+            this.chk_luck.Name = "chk_luck";
+            this.chk_luck.Size = new System.Drawing.Size(91, 17);
+            this.chk_luck.TabIndex = 7;
+            this.chk_luck.Text = "Extreme Luck";
+            this.chk_luck.UseVisualStyleBackColor = true;
             // 
-            // checkBox6
+            // chk_fart
             // 
-            this.checkBox6.AutoSize = true;
-            this.checkBox6.Location = new System.Drawing.Point(126, 51);
-            this.checkBox6.Name = "checkBox6";
-            this.checkBox6.Size = new System.Drawing.Size(97, 17);
-            this.checkBox6.TabIndex = 6;
-            this.checkBox6.Text = "Explosive Farts";
-            this.checkBox6.UseVisualStyleBackColor = true;
+            this.chk_fart.AutoSize = true;
+            this.chk_fart.Location = new System.Drawing.Point(126, 51);
+            this.chk_fart.Name = "chk_fart";
+            this.chk_fart.Size = new System.Drawing.Size(97, 17);
+            this.chk_fart.TabIndex = 6;
+            this.chk_fart.Text = "Explosive Farts";
+            this.chk_fart.UseVisualStyleBackColor = true;
             // 
-            // checkBox7
+            // chk_water_breath
             // 
-            this.checkBox7.AutoSize = true;
-            this.checkBox7.Location = new System.Drawing.Point(126, 74);
-            this.checkBox7.Name = "checkBox7";
-            this.checkBox7.Size = new System.Drawing.Size(103, 17);
-            this.checkBox7.TabIndex = 5;
-            this.checkBox7.Text = "Water Breathing";
-            this.checkBox7.UseVisualStyleBackColor = true;
+            this.chk_water_breath.AutoSize = true;
+            this.chk_water_breath.Location = new System.Drawing.Point(126, 74);
+            this.chk_water_breath.Name = "chk_water_breath";
+            this.chk_water_breath.Size = new System.Drawing.Size(103, 17);
+            this.chk_water_breath.TabIndex = 5;
+            this.chk_water_breath.Text = "Water Breathing";
+            this.chk_water_breath.UseVisualStyleBackColor = true;
             // 
-            // checkBox8
+            // chk_timecontrol
             // 
-            this.checkBox8.AutoSize = true;
-            this.checkBox8.Location = new System.Drawing.Point(126, 97);
-            this.checkBox8.Name = "checkBox8";
-            this.checkBox8.Size = new System.Drawing.Size(88, 17);
-            this.checkBox8.TabIndex = 4;
-            this.checkBox8.Text = "Timer Control";
-            this.checkBox8.UseVisualStyleBackColor = true;
+            this.chk_timecontrol.AutoSize = true;
+            this.chk_timecontrol.Location = new System.Drawing.Point(126, 97);
+            this.chk_timecontrol.Name = "chk_timecontrol";
+            this.chk_timecontrol.Size = new System.Drawing.Size(88, 17);
+            this.chk_timecontrol.TabIndex = 4;
+            this.chk_timecontrol.Text = "Timer Control";
+            this.chk_timecontrol.UseVisualStyleBackColor = true;
+            // 
+            // btn_create
+            // 
+            this.btn_create.Location = new System.Drawing.Point(690, 405);
+            this.btn_create.Name = "btn_create";
+            this.btn_create.Size = new System.Drawing.Size(75, 23);
+            this.btn_create.TabIndex = 3;
+            this.btn_create.Text = "Create Hero";
+            this.btn_create.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_create);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.txt_name);
             this.Controls.Add(this.label1);
@@ -177,14 +188,15 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txt_name;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.CheckBox checkBox2;
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.CheckBox checkBox3;
-        private System.Windows.Forms.CheckBox checkBox5;
-        private System.Windows.Forms.CheckBox checkBox6;
-        private System.Windows.Forms.CheckBox checkBox7;
-        private System.Windows.Forms.CheckBox checkBox8;
-        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox chk_xray;
+        private System.Windows.Forms.CheckBox chk_fly;
+        private System.Windows.Forms.CheckBox chk_invisible;
+        private System.Windows.Forms.CheckBox chk_luck;
+        private System.Windows.Forms.CheckBox chk_fart;
+        private System.Windows.Forms.CheckBox chk_water_breath;
+        private System.Windows.Forms.CheckBox chk_timecontrol;
+        private System.Windows.Forms.CheckBox chk_energy;
+        private System.Windows.Forms.Button btn_create;
     }
 }
 
